bcc1cbe99f2feece2e43129ce7ca0debac0b3035
Gamma <frc5826@gmail.com>
2020-01-07 17:35:23 -0600

    ColorMatch sample code

---
+++ ColorMatcher2/src/main/java/frc/robot/Robot.java
@@ -0,0 +1,96 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.PWMVictorSPX;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+
+import com.revrobotics.ColorSensorV3;
+import com.revrobotics.ColorMatchResult;
+import com.revrobotics.ColorMatch;
+
+import edu.wpi.first.wpilibj.I2C;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.util.Color;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+/**
+ * This is a demo program showing the use of the DifferentialDrive class.
+ * Runs the motors with arcade steering.
+ */
+public class Robot extends TimedRobot {
+  //private final PWMVictorSPX m_leftMotor = new PWMVictorSPX(0);
+  //private final PWMVictorSPX m_rightMotor = new PWMVictorSPX(1);
+  //private final DifferentialDrive m_robotDrive = new DifferentialDrive(m_leftMotor, m_rightMotor);
+  //private final Joystick m_stick = new Joystick(0);
+  private final I2C.Port i2cPort = I2C.Port.kOnboard;
+  private final ColorSensorV3 m_colorSensor = new ColorSensorV3(i2cPort);
+  private final ColorMatch m_colorMatcher = new ColorMatch();
+  private final Color kBlueTarget = ColorMatch.makeColor(0.143, 0.427, 0.429);
+  private final Color kGreenTarget = ColorMatch.makeColor(0.197, 0.561, 0.240);
+  private final Color kRedTarget = ColorMatch.makeColor(0.561, 0.232, 0.114);
+  private final Color kYellowTarget = ColorMatch.makeColor(0.361, 0.524, 0.113);
+  private final PWMVictorSPX leftMotor1 = new PWMVictorSPX(8);
+  private final PWMVictorSPX leftMotor2 = new PWMVictorSPX(9);
+
+  private final PWMVictorSPX rightMotor1 = new PWMVictorSPX(7);
+  private final PWMVictorSPX rightMotor2 = new PWMVictorSPX(6);
+
+  private final SpeedControllerGroup leftSpeedContollers = new SpeedControllerGroup(leftMotor1, leftMotor2);
+  private final SpeedControllerGroup rightSpeedContollers = new SpeedControllerGroup(rightMotor1, rightMotor2);
+  private final DifferentialDrive m_robotDrive = new DifferentialDrive(leftSpeedContollers, rightSpeedContollers);
+
+  
+  @Override
+  public void robotInit() {
+    m_colorMatcher.addColorMatch(kBlueTarget);
+    m_colorMatcher.addColorMatch(kGreenTarget);
+    m_colorMatcher.addColorMatch(kRedTarget);
+    m_colorMatcher.addColorMatch(kYellowTarget);    
+  }
+
+  @Override
+  public void teleopPeriodic() {
+    // Drive with arcade drive.
+    // That means that the Y axis drives forward
+    // and backward, and the X turns left and right.
+    //m_robotDrive.arcadeDrive(m_stick.getY(), m_stick.getX());
+
+    Color detectedColor = m_colorSensor.getColor();
+
+    String colorString;
+    ColorMatchResult match = m_colorMatcher.matchClosestColor(detectedColor);
+
+    if (match.color == kBlueTarget) {
+      colorString = "Blue";
+      m_robotDrive.arcadeDrive(-0.5,0.5);
+    } else if (match.color == kRedTarget) {
+      colorString = "Red";
+      m_robotDrive.arcadeDrive(0.5,-0.5);
+    } else if (match.color == kGreenTarget) {
+      colorString = "Green";
+      m_robotDrive.arcadeDrive(0,0);
+    } else if (match.color == kYellowTarget) {
+      colorString = "Yellow";
+      m_robotDrive.arcadeDrive(0,0.5);
+    } else {
+      colorString = "Unknown";
+    }
+    //System.out.println("The color is "+ colorString);
+
+
+    
+    SmartDashboard.putNumber("Red", detectedColor.red);
+    SmartDashboard.putNumber("Green", detectedColor.green);
+    SmartDashboard.putNumber("Blue", detectedColor.blue);
+    SmartDashboard.putNumber("Confidence", match.confidence);
+    SmartDashboard.putString("Detected Color", colorString);
+
+  }
+}
