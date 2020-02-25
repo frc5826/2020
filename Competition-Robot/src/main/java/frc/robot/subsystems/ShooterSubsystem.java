8881abc995eba038d136301f88fd932e9c2111f3
Beta <frc5826@gmail.com>
2020-02-25 16:28:18 -0600

    Shooter will still shoot if encoder is broken

--- Competition-Robot/src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ Competition-Robot/src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -72,14 +72,19 @@ public class ShooterSubsystem extends SubsystemBase {
     shooterMotor.config_kD(kPIDLoopIdx, kGains_Velocit.kD, kTimeoutMs);
   }
 
-  //TODO - Is the shooter up to speed to shoot
   public boolean isFastEnough() {
-    if(counter++ > 10) {
-      System.out.println(shooterMotor.getSensorCollection().toString());
-      System.out.println(shooterMotor.getSelectedSensorVelocity(kPIDLoopIdx));
+    int sensorVelocity = shooterMotor.getSelectedSensorVelocity(kPIDLoopIdx);
+
+    // Note - getSelectedSensorVelocity returns 0 when encoder disconnected
+    // If getSelectedSensorVelocity returns 0 for 5?? loops then just return true
+    if(sensorVelocity == 0 && counter++ > 5){
+      return true;
+    } else if (sensorVelocity > 0){
+      // Only reset the counter if the sensor is working
       counter = 0;
     }
-    return shooterMotor.getSelectedSensorVelocity(kPIDLoopIdx) > kShootRPM * .95;
+
+    return sensorVelocity > kShootRPM * .95;
   }
 
   @Override
@@ -129,7 +134,6 @@ public class ShooterSubsystem extends SubsystemBase {
 
   //Min 0.0, Max = 1.0
   public double spinShoot(){
-    //TODO - What if you encoder gets unplugged?
     //100 is full speed
     //10 is RPM
     //8192 is count per full revolution
